2b5aaf579f487ee79ab274c3d7ff0b3a066c3323
MaxYurs <maxyurs@gmail.com>
2019-07-23 18:52:42 -0500

    added basic keypad functionality

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
-from pad4pi import rpi_gpio
+#!/usr/bin/env python3.7
 
+from pad4pi import rpi_gpio
+import time
+
+# Setup keypad
 KEYPAD = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    ["*", 0, "#"]
+    [1, 2, 3, "A"],
+    [4, 5, 6, "B"],
+    [7, 8, 9, "C"],
+    ["*", 0, "#","D"]
 ]
-#dope new comment
-ROW_PINS = [4, 14, 15, 17] # BCM numbering
-COL_PINS = [18, 27, 22] # BCM numbering
+
+ROW_PINS = [24, 22, 27, 18] # BCM numbering
+COL_PINS = [17, 15, 14, 4] # BCM numbering
 
 factory = rpi_gpio.KeypadFactory()
 
 # Try factory.create_4_by_3_keypad
 # and factory.create_4_by_4_keypad for reasonable defaults
 keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
+loop_bool = True
 
-def printKey(key):
-    print(key)
+class KeyStore:
+	def __init__(self):
+		# Empty list to store characters
+		self.pressed_keys = []
+
+	def store_key(self, key):
+		if(key=='#'):
+			print(self.pressed_keys)
+			self.clear_keys()
+		else:
+			print(key)
+			self.pressed_keys.append(key)
+
+	def clear_keys(self):
+		self.pressed_keys.clear()
+
+#def printKey(key):
+#    print(key)
+
+keys = KeyStore()
 
 # printKey will be called each time a keypad button is pressed
-keypad.registerKeyPressHandler(printKey)
+keypad.registerKeyPressHandler(keys.store_key)
+
+print("Hello Max\n")
+
+while(loop_bool):
+	time.sleep(0.2)
+
+keypad.cleanup()
